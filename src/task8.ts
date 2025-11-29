a79467467ecf6952997b022f9ea4a5c286975342
Kaveewison Kritsadawiwat <kaveewison_k@cmu.ac.th>
2025-11-29 16:53:49 +0700

    Grade

--- src/task8.ts
+++ src/task8.ts
@@ -1,8 +1,19 @@
-let x:number = 10
-let y:number = 20
-if (x > 5 && y > 12) {
-    console.log('ค่ามากจัง')
-}else {
-    console.log(x*y + x/y)
-}
+// let x:number = 10
+// let y:number = 20
+// if (x > 5 && y > 12) {
+//     console.log('ค่ามากจัง')
+// }else {
+//     console.log(x*y + x/y)
+// }
 
+let x =120
+if (x > 80) {
+    console.log('Grade A')
+} else if (x > 60 && x < 80){
+    console.log('Grade B')
+} else if (x > 50 && x < 60){
+    console.log('Grade C')
+}
+else {
+    console.log('Grade F')
+}
